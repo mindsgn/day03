7668f754c2092ea085ba04bf689ffd65bde95313
Sibongiseni Tembe <stembe@c3r2s1.wethinkcode.co.za>
2020-06-20 15:15:19 +0200

    update

--- ex02/ft_ultimate.ft.c
+++ ex02/ft_ultimate.ft.c
@@ -1,19 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_ultimate.ft.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stembe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/20 14:31:49 by stembe            #+#    #+#             */
-/*   Updated: 2020/06/20 14:57:37 by stembe           ###   ########.fr       */
+/*   Created: 2020/06/20 14:58:39 by stembe            #+#    #+#             */
+/*   Updated: 2020/06/20 15:13:39 by stembe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-
-void ft_ft(int *nbr){
+void ft_ultimate_ft(int *********nbr){
+	int *ptr;
+	int **ptr2;
+	int ***ptr3
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
+	int *********ptr8;
+		
 	int number = 42;
-   	nbr = &number;	
+	
+	ptr = &number;
+	ptr2 = &ptr;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	nbr9 = &ptr8;
 }
